a943fccfe80edb32c9fb54867d4d63413c6ecb1a
Sherri Mitchell <rsamitchell@att.net>
2015-06-08 12:37:13 -0400

    Added updated homework files.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,9 @@
 source 'https://rubygems.org'
 
-# Specify your gem's dependencies in etsiest.gemspec
 gemspec
+gem 'sinatra'
+gem 'etsy'
+
+group :development do
+  gem 'pry'
+end
--- lib/etsiest.rb
+++ lib/etsiest.rb
@@ -1,5 +1,24 @@
 require "etsiest/version"
+require 'sinatra/base'
+require 'pry'
+require 'etsy'
+
+Etsy.api_key = ENV['ETSY_KEY']
 
 module Etsiest
-  # Your code goes here...
+  class EtsySearch < Sinatra::Base
+get '/etsy_search'  do
+  @results = Etsy::Request.get('/listings/active', 
+    :includes => ['Images', 'Shop'], 
+    :keywords => 'furniture',
+    :limit => 25,
+    :offset =>  25)
+  erb :index
+
+  run! if app_file == $0
+  end
 end
+
+# set :library, Etsiest.new(song_path). 
+
+# Etsy::Request.get('/listings/active', :includes => ['Images', 'Shop'], :keywords => 'whiskey')
